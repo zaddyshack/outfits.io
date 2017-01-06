c7b1a1f3daecc14678b48363f52d900b1a39316f
WillyWonka125 <william.m.erickson@gmail.com>
2017-01-06 17:57:21 -0600

    First commit
    Create index.php, that is all.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <style>
+      .container{
+        background-color:white;
+        color:black;
+        margin:50px;
+        padding:25px;
+    </style>
+  </head>
+
+  <body>
+    <div class="container">
+      <h1>Outfit Selector PRO v1</h1>
+    </div>
+    
+    <div class="container>
+      <p><?php
+        echo "Smd bro";
+        >?</p>
+      
+    </div>
+    
+  </body>
+</html>
